1a774ad88064b4af1c92efe814cbd24ffbdc4d88
Emily Liu <emilyliu3579@gmail.com>
2024-08-08 22:14:03 -0700

    Initial Commit

---
+++ ProductSustainability/ProductSustainabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductSustainabilityApp.swift
+//  ProductSustainability
+//
+//  Created by Emily Liu on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductSustainabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
